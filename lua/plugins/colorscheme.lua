ee0e0e8858ea056215045483ab655c8fcff3423c
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2025-09-15 22:27:29 +0530

    update: add tokyonight colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,27 +1,30 @@
--- return {
---   "luisiacc/gruvbox-baby",
---   priority = 1000,
---   config = function()
---     vim.cmd([[colorscheme gruvbox-baby]])
---   end,
--- }
-
 return {
-  "sainnhe/gruvbox-material",
-  enabled = true,
-  priority = 1000,
-  config = function()
-    vim.g.gruvbox_material_transparent_background = 0
-    vim.g.gruvbox_material_foreground = "mix"
-    vim.g.gruvbox_material_background = "hard"
-    vim.g.gruvbox_material_ui_contrast = "high"
-    vim.g.gruvbox_material_float_style = "bright"
-    vim.g.gruvbox_material_statusline_style = "material"
-    vim.g.gruvbox_material_cursor = "auto"
+  {
+    "sainnhe/gruvbox-material",
+    enabled = false,
+    priority = 1000,
+    config = function()
+      vim.g.gruvbox_material_transparent_background = 0
+      vim.g.gruvbox_material_foreground = "mix"
+      vim.g.gruvbox_material_background = "hard"
+      -- vim.g.gruvbox_material_ui_contrast = "high"
+      -- vim.g.gruvbox_material_float_style = "bright"
+      -- vim.g.gruvbox_material_statusline_style = "material"
+      -- vim.g.gruvbox_material_cursor = "auto"
 
-    -- vim.g.gruvbox_material_colors_override = { bg0 = '#16181A' } -- #0e1010
-    -- vim.g.gruvbox_material_better_performance = 1
+      -- vim.g.gruvbox_material_colors_override = { bg0 = '#16181A' } -- #0e1010
+      -- vim.g.gruvbox_material_better_performance = 1
 
-    vim.cmd.colorscheme("gruvbox-material")
-  end,
+      vim.cmd.colorscheme("gruvbox-material")
+    end
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function() 
+      vim.cmd[[colorscheme tokyonight-night]]
+    end
+  }
 }
