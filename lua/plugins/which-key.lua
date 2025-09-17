c553bd987a8e42519e539879bd7b1a090e9137f8
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2025-09-17 22:20:14 +0530

    update: add icons for which-key

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,6 +1,9 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
+  dependencies = {
+    "nvim-mini/mini.nvim"
+  },
   init = function()
     vim.o.timeout = true
     vim.o.timeoutlen = 500
